ad39bd29be76cb6caca5ab8fe6d645d570bf2565
YaKov <45480231+TyutterinYakov@users.noreply.github.com>
2021-12-07 01:00:16 +0300

    Add files via upload
    pom.xml maven. sql DB create and connect

---
+++ createDB.sql
@@ -0,0 +1,11 @@
+create table contacts(
+	contact_id SERIAL,
+	sur_name varchar(100) not null,
+	given_name varchar(100) not null,
+	contact_age int not null,
+	user_num varchar(30) not null,
+	user_email varchar(64)
+);
+
+
+
